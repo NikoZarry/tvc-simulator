d1d3fe6349cb03e93237d42493f3202dad360e18
Hidekel Irizarry <nikozarry@gmail.com>
2026-07-16 13:55:54 +0000

    feat: Added constants.py, including real data obtained from thrustcurve.org

---
+++ constants.py
@@ -0,0 +1,16 @@
+# Thrust Curve constants (AeroTech J350W)
+MOTOR_MASS = 0.665    # kg, mass of the motor
+PROP_MASS = 0.375     # kg, mass of the propellant
+BURN_TIME = 1.5       # s, time it takes to use all the propellant
+AVG_THRUST = 445      # N, average thrust throughout ascent
+
+# Rocket-specific estimates
+DRY_ROCKET_MASS = 4   # kg, PLACEHOLDER: airframe + recovery + payload, no motor or propellant
+
+# Derived constants
+INITIAL_MASS = DRY_ROCKET_MASS + MOTOR_MASS   # kg, mass of the entire rocket at liftoff
+BURN_RATE = PROP_MASS / BURN_TIME             # kg/s, rate at which propellant is expelled
+
+# Simulation / environment constants
+GRAVITY = -9.81       # m/s^2
+DT = 0.01             # s, 100 checks per second (100 Hz)
