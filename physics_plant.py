114ae23fc70a13c5035412ce8a7336fc0187bfac
Hidekel Irizarry <nikozarry@gmail.com>
2026-08-06 19:57:01 +0000

    Add comprehensive rocket print out

--- physics_plant.py
+++ physics_plant.py
@@ -1,6 +1,5 @@
 """
 Add intermediate rocket physics loop:
-- add air drag
 - add better print out
 
 ASSUMPTIONS:
@@ -9,6 +8,7 @@ ASSUMPTIONS:
 
 # Imports
 import constants as c   # Imported from constants.py
+import math
 
 
 rocket_state = {            # rocket's starting parameters 
@@ -19,8 +19,11 @@ rocket_state = {            # rocket's starting parameters
   "sim_time": 0             # s, timer hasn't started yet
 }
 
-state_list = []             # will be used to store a "snapshot" of the rocket's state
-                            # at each interval "dt"
+state_list = []                      # will be used to store a "snapshot" of the rocket's state
+                                     # at each interval "dt"
+
+step_count = 0                                      # tracks total number of step() calls
+steps_per_second = round(c.PRINT_INTERVAL / c.DT)   # e.g. 100 when dt = 0.01
 
 # Functions
 def massCalculation(mass):                    # Decreases mass due to propellant being expelled
@@ -37,7 +40,7 @@ def forceCalculation(mass, drag, time):                # Calculates net forces a
   if time <= c.BURN_TIME:                       
     return c.AVG_THRUST + (mass * c.GRAVITY) + drag    # Whilst there's propellant to burn, force is equal to
   else:                                                # Thrust + (Force due to gravity)
-    return (mass * c.GRAVITY) + drag                   # When propellant is out, only force is gravity (drag will be added in a later commit)
+    return (mass * c.GRAVITY) + drag                   # When propellant is out, only forces acting are gravity and drag
 
 def accelCalculation(mass, force):                                      # Calculates acceleration due to net forces (F=ma)
   return force / mass                                                   # a = F/m
@@ -70,29 +73,57 @@ def step(state):                        # "step" function that runs every time s
   state["velocity"] = final_velocity
   state["acceleration"] = net_acceleration          
   state["sim_time"] = time + c.DT                   # increments time by dt (0.01 s)
+  
+  global step_count
+  step_count += 1
 
   state_list.append(state.copy())                   # adds a copy of the rocket's previous state to the "state_list" list
 
 
+def print_out():                                    # Prints out a line displaying height, velocity, accel, and elapsed time 
+  height = rocket_state["height"]
+  velocity = rocket_state["velocity"]
+  accel = rocket_state["acceleration"]
+  time = rocket_state["sim_time"]
+
+  print(f"height = {height:>8.3f} m", end="  |  ")
+  print(f"velocity = {velocity:>8.3f} m/s", end="  |  ")
+  print(f"acceleration = {accel:>8.3f} m/s^2", end="  |  ")
+  print(f"time elapsed = {time:>4.1f} s")
+
+print("--------------------------------------- POWERED FLIGHT (BURN PHASE) ---------------------------------------")
+
 step(rocket_state)                        # Runs a quick step in order to make the rocket's height non-zero
                                           # Since the condition below runs whilst the rocket is airborne
+
 while rocket_state["height"] >= 0:        # Keeps incrementing time until the rocket reaches the ground
-  step(rocket_state)                    
+
+  step(rocket_state)
+  
+  if step_count % steps_per_second == 0:
+    print_out()
+
+  if round(rocket_state["sim_time"], 2) == c.BURN_TIME:
+    print("--------------------------------- BURN OUT (PROPELLANT HAS BEEN DEPLETED) ---------------------------------")
+
+  if round(rocket_state["velocity"], 2) == 0:
+    print("------------------------------------------ DESCENT (VELOCITY < 0) ------------------------------------------")
+
 
 # Interpolated final state
 def interpolation(x0, x1, alpha):              # Blends between two values using alpha as the fraction between them
-  return x0 + alpha * (x1 - x0)                 # x = x0 + alpha * (x1 - x0), general linear interpolation
+  return x0 + alpha * (x1 - x0)                # x = x0 + alpha * (x1 - x0), general linear interpolation
 
 
-def final_state():                                                            # Corrects the overshot landing state using linear interpolation
+def final_state():                                                                    # Corrects the overshot landing state using linear interpolation
 
-  state0 = state_list[-2]                                                     # Last logged state still at or above ground (before the crossing)
-  state1 = state_list[-1]                                                     # First logged state that overshot below ground (after the crossing)
+  state0 = state_list[-2]                                                             # Last logged state still at or above ground (before the crossing)
+  state1 = state_list[-1]                                                             # First logged state that overshot below ground (after the crossing)
 
-  alpha = (0 - state0["height"]) / (state1["height"] - state0["height"])      # Fraction of the timestep where height actually crossed 0
-  rocket_state["height"] = 0                                                  # True landing height, exactly ground level
+  alpha = (0 - state0["height"]) / (state1["height"] - state0["height"])              # Fraction of the timestep where height actually crossed 0
+  rocket_state["height"] = 0                                                          # True landing height, exactly ground level
 
-  final_time = interpolation(state0["sim_time"], state1["sim_time"], alpha)   # Interpolated sim_time at the true moment of landing
+  final_time = interpolation(state0["sim_time"], state1["sim_time"], alpha)           # Interpolated sim_time at the true moment of landing
   rocket_state["sim_time"] = final_time
 
   final_velocity = interpolation(state0["velocity"], state1["velocity"], alpha)       # Interpolated velocity at the true moment of landing
@@ -101,14 +132,25 @@ def final_state():                                                            # 
   final_accel = interpolation(state0["acceleration"], state1["acceleration"], alpha)  # Interpolated acceleration at the true moment of landing
   rocket_state["acceleration"] = final_accel
 
+  print("------------------------------------------- LANDING (HEIGHT = 0) -------------------------------------------")
+  print(f"height = {rocket_state["height"]:>8.3f} m", end="  |  ")
+  print(f"velocity = {rocket_state["velocity"]:>8.3f} m/s", end="  |  ")
+  print(f"acceleration = {rocket_state["acceleration"]:>8.3f} m/s^2", end="  |  ")
+  print(f"time elapsed = {rocket_state["sim_time"]:>4.1f} s")
+  print("------------------------------------------------------------------------------------------------------------")
 
 
-final_state()                                    # Runs the interpolation, replaces rocket_state's overshot values with the true landing state
+apogee_state = max(state_list, key = lambda entry: entry["height"])                             # Rocket state where max height was achieved
+maxv_state = max(state_list, key = lambda entry: entry["velocity"])                             # Rocket state where max velocity was achieved
+maxa_state = max(state_list, key = lambda entry: entry["acceleration"])                         # Rocket state where max acceleration was achieved
+burnout_list = [entry for entry in state_list if round(entry["sim_time"], 2) == c.BURN_TIME]    # A list of rocket states where elapsed time is = burnout time (just one state)
+burnout_state = burnout_list[0]                                                                 # Rocket state where the propellant was completely depleted
+
+final_state()       # Runs the interpolation, replaces rocket_state's overshot values with the true landing state
 
 # PRINT OUT
-print(f"\nFinal height: {rocket_state["height"]:.3f} m")  
-print(f"\nFinal vertical veloctiy : {rocket_state["velocity"]:.3f} m/s")
-print(f"\nFinal vertical acceleration: {rocket_state["acceleration"]:.3f} m/s^2")
-print(f"\nFinal rocket mass: {rocket_state["mass"]:.3f} kg")
-print(f"\nTheoretical final rocket mass: {c.FINAL_MASS:.3f} kg")
-print(f"\nFinal sim_time: {rocket_state["sim_time"]:.3f} s")
+print("\n\n\n------------------- FINAL PRINT OUT -------------------")
+print(f"Apogee Height: {apogee_state["height"]:.3f} m, reached at {apogee_state["sim_time"]:.2f} s")  
+print(f"\nMax Velocity: {maxv_state["velocity"]:.3f} m/s, reached at {maxv_state["sim_time"]:.2f} s")
+print(f"\nMax Acceleration: {maxa_state["acceleration"]:.3f} m/s^2, reached at {maxa_state["sim_time"]:.2f} s")
+print(f"\nBurnout Height: {burnout_state["height"]:.3f} m, Burnout Velocity: {burnout_state["velocity"]:.3f} m/s, reached at {burnout_state["sim_time"]:.2f} s")
