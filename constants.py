e1f5e65201962516672cbd2dae9f0002a2c30790
Hidekel Irizarry <nikozarry@gmail.com>
2026-08-11 18:17:45 +0000

    refactor: corrected wrong values for certain constants and correctly adjusted pitch to delta

--- constants.py
+++ constants.py
@@ -7,18 +7,18 @@ GRAVITY = -9.81       # m/s^2
 DT = 0.01             # s, 100 checks per second (100 Hz)
 PRINT_INTERVAL = 0.5  # Determines how often a print out occurs
 AIR_DENSITY = 1.225   # kg/m^3, standard sea-level air density
-PITCH_ANGLE = 5       # degrees, how far the rocket's body axis has rotated off the vertical
+DELTA = 5             # degrees, angle of the nozzle with respect to the rocket's axis
 
 # Thrust Curve constants (AeroTech J350W)
 PROP_MASS = 0.375     # kg, mass of the propellant
 MOTOR_MASS = 0.665    # kg, mass of the motor (including propellant)
-BURN_TIME = 2.0       # s, time it takes to use all the propellant
-AVG_THRUST = 500      # N, average thrust throughout ascent
+BURN_TIME = 1.5       # s, time it takes to use all the propellant
+AVG_THRUST = 445      # N, average thrust throughout ascent
 
 # Rocket-specific estimates (PLACEHOLDERS)
-DRY_ROCKET_MASS = 6     # kg, airframe + recovery + payload, no motor or propellant
+DRY_ROCKET_MASS = 4     # kg, airframe + recovery + payload, no motor or propellant
 DRAG_COEFFICIENT = 0.5  # dimeonsionless, represents how much a shape resists air flow
-DIAMETER = 0.150        # m, diameter of the rocket's tube body
+DIAMETER = 0.102        # m, diameter of the rocket's tube body
 LENGTH = 1.8            # m, length of the rocket from tail to nose
 
 # Derived constants
